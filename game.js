ff5da15aee958d452b8cea1255f5a91af9ebdf01
Caleb <100630232+Calebroe@users.noreply.github.com>
2023-08-24 14:20:51 -0400

    adding foundational logic

--- game.js
+++ game.js
@@ -1,17 +1,28 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js'
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
+import { update as updateFood, draw as drawFood } from './food.js'
+import { outsideGrid } from './grid.js'
 
 let lastRenderTime = 0
-
+let gameOver = false
 const gameBoard = document.getElementById('game-board')
-function main(currentTime) {
-  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
-  if (secondsSinceLastRender < (1 / SNAKE_SPEED)) return
-  window.requestAnimationFrame(main)
-  lastRenderTime = currentTime
-  //console.log(currentTime)
-  //console.log('Render')
 
-  update() 
+function main(currentTime) {
+  if (gameOver) {
+    if (confirm('You lost. Press ok to restart.')) {
+      window.location = '/'
+    }
+    return
+  }
+
+
+  window.requestAnimationFrame(main)
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+
+  lastRenderTime = currentTime
+
+  update()
   draw()
 }
 
@@ -19,8 +30,16 @@ window.requestAnimationFrame(main)
 
 function update() {
   updateSnake()
+  updateFood()
+  checkDeath()
 }
 
 function draw() {
+  gameBoard.innerHTML = ''
   drawSnake(gameBoard)
+  drawFood(gameBoard)
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
